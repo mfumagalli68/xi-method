de8998892f661ac3433d8b5909b414af735c6a2c
marco.fumagalli <marco.fumagalli@esprinet.com>
2023-08-23 16:05:47 +0200

    tests

--- tests/test_xi.py
+++ tests/test_xi.py
@@ -43,3 +43,20 @@ def test_separation_measurement_m_dict():
     p = xi.explain(X=df, y=Y, replicates=1, separation_measurement='L1')
 
     assert isinstance(p.get('L1').value, np.ndarray)
+
+def test_separation_measurement_regressor():
+
+    np.random.seed(2)
+    Y = np.random.normal(size=1000)
+    X = np.random.normal(3, 2, size=5*1000)#df_np[:, 1:11]
+    X = X.reshape((1000, 5))
+
+    xi = XIRegressor(m=10,grid=100)
+    p = xi.explain(X=X, y=Y, replicates=1, separation_measurement='Kullback-leibler')
+
+    exp = np.array(
+        [0.065,0.05,0.065,0.061,0.05])
+    np.testing.assert_allclose(p.get('Kullback-leibler').value,
+                               exp,
+                               rtol=0.05,
+                               atol=0.05)
--- xi/__init__.py
+++ xi/__init__.py
@@ -1,28 +1,4 @@
 import logging
 
-def enum(enum_type='enum', base_classes=None, methods=None, **attrs):
-    """
-    Generates a enumeration with the given attributes.
-    """
-
-    # Enumerations can not be initalized as a new instance
-    def __init__(instance, *args, **kwargs):
-        raise RuntimeError('%s types can not be initialized.' % enum_type)
-
-    if base_classes is None:
-        base_classes = ()
-
-    if methods is None:
-        methods = {}
-
-    base_classes = base_classes + (object,)
-    for k, v in methods.items():
-        methods[k] = classmethod(v)
-
-    attrs['enums'] = attrs.copy()
-    methods.update(attrs)
-    methods['__init__'] = __init__
-    return type(str(enum_type), base_classes, methods)
-
 
 logging.getLogger().setLevel(logging.INFO)
