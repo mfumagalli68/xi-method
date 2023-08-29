b45b5dda5c37486d90288d50e2f81807cb4c512a
marco.fumagalli <marco.fumagalli@esprinet.com>
2023-08-29 22:30:59 +0200

    example

---
+++ example/example.py
@@ -0,0 +1,10 @@
+from xi.ximp import *
+
+if __name__ == '__main__':
+    Y = np.random.choice(np.array([1, 2, 3]), size=1000)
+    X = np.random.normal(2, 2, size=4 * 1000)  # df_np[:, 1:11]
+    X = X.reshape((1000, 4))
+    #         start_time = time.time()
+    xi = XIClassifier(m=10)
+    p = xi.explain(X=X, y=Y, replicates=1, separation_measurement='Kullback-leibler')
+    print(p.get('Kullback-leibler').explanation)
